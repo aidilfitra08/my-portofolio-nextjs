7227911d2d163a4e8f72f6a0cdaa6da3f3a94a2c
aidilfitra08 <aidil.gitu1@gmail.com>
2025-02-13 18:16:03 +0700

    update responsive navbar

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const [bgColor, setBgColor] = useState(false);
+  const [navMobile, setNavMobile] = useState(false);
   const listenScrollEvent = (e: Event) => {
     const window = e.currentTarget as Window;
     if (window.scrollY > 10) {
@@ -20,18 +23,21 @@ export default function Navbar() {
     <nav
       className={`${
         bgColor
-          ? " bg-black bg-opacity-90 text-white dark:bg-neutral-800 dark:bg-opacity-90"
+          ? " bg-black bg-opacity-90 text-white dark:bg-neutral-800 dark:bg-opacity-80"
           : ""
-      } ease-out duration-300 fixed inset-x-0 w-screen h-16 flex flex-row text-center sm:flex-row sm:text-left justify-between items-center z-40 pr-2 sm:pr-10 font-mono`}
+      } ease-out duration-300 fixed inset-x-0 w-screen h-16 flex flex-row text-center sm:flex-row sm:text-left justify-between items-center z-40 pr-2 sm:pr-10 font-mono backdrop-blur-sm`}
     >
       <div className="flex">
         <div className=" px-10 h-full ">
           <Link className=" font-bold text-4xl" href="/">
-            Logo
+            AidilDev
           </Link>
         </div>
       </div>
-      <div className="flex content-center h-full ">
+      <div className="md:hidden" onClick={() => setNavMobile(true)}>
+        <FontAwesomeIcon icon={faBars} size="xl" className="pr-8" />
+      </div>
+      <div className="flex content-center h-full max-md:hidden">
         <p
           className={`${
             bgColor
@@ -80,6 +86,71 @@ export default function Navbar() {
             Contact Me
           </Link>
         </p>
+      </div>
+      <div
+        className={`${
+          navMobile ? "left-0" : " left-full"
+        } top-0  grid-cols-1 fixed  bg-neutral-900 h-screen grid items-center w-screen bg-opacity-95 transition-all duration-300 ease-in`}
+      >
+        <div
+          className="absolute right-2 top-5 text-1xl"
+          onClick={() => setNavMobile(false)}
+        >
+          <FontAwesomeIcon icon={faXmark} size="2xl" className="pr-8" />
+        </div>
+        <div className="text-1xl">
+          <p
+            className={`${
+              bgColor
+                ? " hover:text-black "
+                : " hover:text-white dark:hover:text-black "
+            } `}
+          >
+            <Link
+              href="#about-me"
+              className={`${
+                bgColor ? " hover:bg-white " : ""
+              }px-5 hover:bg-black dark:hover:bg-white align-middle text-center ease-in duration-200 h-16 table-cell w-screen`}
+              onClick={() => setNavMobile(false)}
+            >
+              About me
+            </Link>
+          </p>
+          <p
+            className={`${
+              bgColor
+                ? " hover:text-black "
+                : "hover:text-white dark:hover:text-black"
+            }`}
+          >
+            <Link
+              href="#projects"
+              className={`${
+                bgColor ? " hover:bg-white " : ""
+              }px-5 hover:bg-black dark:hover:bg-white align-middle text-center  ease-in duration-200 h-16 table-cell w-screen`}
+              onClick={() => setNavMobile(false)}
+            >
+              Projects
+            </Link>
+          </p>
+          <p
+            className={`${
+              bgColor
+                ? " hover:text-black "
+                : "hover:text-white dark:hover:text-black"
+            }`}
+          >
+            <Link
+              href="#footer"
+              className={`${
+                bgColor ? " hover:bg-white " : ""
+              }px-5 hover:bg-black dark:hover:bg-white align-middle text-center  ease-in duration-200 h-16 table-cell w-screen`}
+              onClick={() => setNavMobile(false)}
+            >
+              Contact Me
+            </Link>
+          </p>
+        </div>
       </div>
     </nav>
   );
