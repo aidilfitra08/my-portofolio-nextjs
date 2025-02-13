5c700e5f113760dfe26d9f95f2dc5a4bf69cd50c
aidilfitra08 <aidil.gitu1@gmail.com>
2025-02-13 18:16:18 +0700

    add work component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import AboutMe from "./components/aboutme/aboutme";
 import Skill from "./components/skill/skill";
 import Projects from "./components/projects/projects";
+import Work from "./components/work/work";
 
 export default function Home() {
   return (
     <div>
       <AboutMe />
       <Skill />
+      <Work />
       <Projects />
     </div>
   );
