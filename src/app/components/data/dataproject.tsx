2444eedb724cf2e6ace4a3c92e5ebe658ee6a663
aidilfitra08 <aidil.gitu1@gmail.com>
2025-02-13 18:15:30 +0700

    add data jsx

---
+++ src/app/components/data/dataproject.tsx
@@ -0,0 +1,20 @@
+export const dataProject = [
+  {
+    id: 0,
+    namaProject: "Learning Management System",
+    detail:
+      "Final project about Learning Management System for digital marketing training. This project aims to develop digital marketing for Universitas Padjadjaran students.",
+    link: "#about-me",
+    onlyImage: true,
+    image: "/lms.png",
+  },
+  {
+    id: 1,
+    namaProject: "Learning Management System",
+    detail:
+      "Final project about Learning Management System for digital marketing training. This project aims to develop digital marketing for Universitas Padjadjaran students.",
+    link: "#about-me",
+    onlyImage: true,
+    image: "/lms.png",
+  },
+];
