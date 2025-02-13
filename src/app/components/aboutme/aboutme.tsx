5066e289de3a5eef16d84b16d175b11677646357
aidilfitra08 <aidil.gitu1@gmail.com>
2025-02-13 18:17:12 +0700

    responsive about me

--- src/app/components/aboutme/aboutme.tsx
+++ src/app/components/aboutme/aboutme.tsx
@@ -1,20 +1,22 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function AboutMe() {
   return (
-    <div className="pt-16 grid grid-cols-2  h-[700px] items-center font-mono">
-      <div className="col-span-1 p-28">
-        <p className="text-6xl font-bold">
+    <div className="pt-16 grid grid-cols-2  h-[700px] items-center font-mono relative max-md:h-[600px] overflow-hidden">
+      <div className="z-10 col-span-1 p-28 grid grid-cols-1 max-md:col-span-2 max-md:px-10 max-md:py-16 ">
+        <p className="text-6xl font-bold col-span-1 max-md:text-5xl">
           Hello, <br /> I am <span className="font-extrabold">Aidil</span>
         </p>
-        <p>
+        <p className="col-span-1 mt-6">
           I&apos;m a Computer Science graduate with experience in backend, web,
           and mobile development. I&apos;m skilled in technologies like
           CodeIgniter, Flutter, Node.js, React, Golang, MySQL, and MongoDB.
         </p>
         {/* <div>My main skill</div> */}
-        <p className="mt-6">
+        <p className=" col-span-1 mt-8">
           <Link
             href="https://drive.google.com/file/d/1pqhiifwNNwLfHYseyYW-aAYH52E78ms3/view?usp=sharing"
             className=" bg-neutral-800 text-white p-3 hover:bg-white hover:text-black ease-in-out duration-300"
@@ -22,15 +24,30 @@ export default function AboutMe() {
             Curriculum Vitae
           </Link>
         </p>
+        <div className="col-span-1 flex mt-8 space-x-2">
+          <Link className="" href="https://www.linkedin.com/in/aidil-fitra">
+            {" "}
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size="2xl"
+              className="pr-2"
+            />{" "}
+          </Link>
+          <Link className="" href="https://github.com/aidilfitra08">
+            {" "}
+            <FontAwesomeIcon icon={faGithub} size="2xl" className="pr-2" />{" "}
+          </Link>
+        </div>
       </div>
-      <div className="col-span-1 object-fill overflow-hidden h-full">
+      <div className="z-0 col-span-1 object-fill overflow-hidden h-full max-md:absolute max-md:top-[165px]">
         <Image
           aria-hidden
           src="/me.png"
           alt="Globe icon"
           width={1000}
           height={1000}
-          className="mt-16"
+          className="mt-16 max-md:opacity-20"
+          priority={true}
         />
       </div>
     </div>
--- src/app/components/skill/skill.tsx
+++ src/app/components/skill/skill.tsx
@@ -1,21 +1,22 @@
 export default function Skill() {
   return (
     <div className="grid grid-cols-2 font-mono font-medium bg-neutral-800 text-white">
-      <div className="col-span-1 grid grid-cols-5 text-center gap-3 p-16 items-start h-fit">
-        <p className="col-span-5 text-3xl">Skills</p>
-        <p className=" bg-neutral-700 p-2 rounded-lg">Node.js</p>
-        <p className=" bg-neutral-700 p-2 rounded-lg">React.js</p>
-        <p className=" bg-neutral-700 p-2 rounded-lg">Golang</p>
-        <p className=" bg-neutral-700 p-2 rounded-lg">MySQL</p>
-        <p className=" bg-neutral-700 p-2 rounded-lg">MongoDB</p>
-        <p className=" bg-neutral-700 p-2 rounded-lg">PostgreSQL</p>
-        <p className=" bg-neutral-700 p-2 rounded-lg">Codeigniter</p>
-        <p className=" bg-neutral-700 p-2 rounded-lg">Next.js</p>
+      <div className="col-span-1 grid grid-cols-3 text-center gap-3 p-16 max-md:px-10 items-start h-fit max-md:col-span-2 max-md:grid-cols-3 ">
+        <p className="col-span-3 text-3xl font-bold text-start">Skills</p>
+        <div className="col-span-3 flex gap-x-2 gap-y-2 align-middle max-md:w-[280px] flex-wrap">
+          <p className=" bg-neutral-700 p-2 rounded-lg">Node.js</p>
+          <p className=" bg-neutral-700 p-2 rounded-lg">React.js</p>
+          <p className=" bg-neutral-700 p-2 rounded-lg">Golang</p>
+          <p className=" bg-neutral-700 p-2 rounded-lg">MySQL</p>
+          <p className=" bg-neutral-700 p-2 rounded-lg">MongoDB</p>
+          <p className=" bg-neutral-700 p-2 rounded-lg">PostgreSQL</p>
+          <p className=" bg-neutral-700 p-2 rounded-lg">Codeigniter</p>
+          <p className=" bg-neutral-700 p-2 rounded-lg">Next.js</p>
+        </div>
       </div>
-      <div className="col-span-1 grid grid-cols-1 p-16 space-y-3">
-        <p className="col-span-1 text-3xl">Education & Work Experience</p>
+      <div className="col-span-1 grid grid-cols-1 p-16 max-md:pt-0 space-y-3 max-md:col-span-2 max-md:px-10">
+        <p className="col-span-1 text-3xl font-bold">Education</p>
         <div className="col-span-1 grid grid-cols-1 ">
-          <p className="col-span-1 text-xl">Education</p>
           <ul className="list-disc ml-5">
             <li>
               <p>
@@ -25,18 +26,6 @@ export default function Skill() {
                 Bachelor of Computer Science, <span>3.60/4.00</span>
               </p>
               <p>August 2019 - August 2024</p>
-            </li>
-          </ul>
-        </div>
-        <div className="col-span-1 grid grid-cols-1 mt-4">
-          <p className="col-span-1 text-xl">Work Experience</p>
-          <ul className="list-disc ml-5">
-            <li>
-              <p>
-                Vocasia - <span>Indonesia</span>
-              </p>
-              <p>Back End Developer Intern</p>
-              <p>February 2022 - July 2022</p>
             </li>
           </ul>
         </div>
