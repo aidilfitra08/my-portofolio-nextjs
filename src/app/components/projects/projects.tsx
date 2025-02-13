849edace54e02958ea4df1011b866d9050ad1ccc
aidilfitra08 <aidil.gitu1@gmail.com>
2025-02-13 18:16:52 +0700

    sync project

--- src/app/components/projects/projects.tsx
+++ src/app/components/projects/projects.tsx
@@ -1,152 +1,104 @@
 "use client";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
+import { dataProject } from "../data/dataproject";
 
 export default function Projects() {
+  // let dataProjects = dataProject;
   const [onlyImage, setOnlyImage] = useState({
-    image1: true,
-    image2: true,
-    image3: true,
+    image1: dataProject[0].onlyImage,
+    image2: dataProject[1].onlyImage,
   });
   return (
     <div
-      className="grid grid-cols-1 h-fit py-16 px-16 space-y-6 justify-center"
+      className="grid grid-cols-1 h-fit py-16 px-10 space-y-6 justify-center font-mono"
       id="projects"
     >
-      <p className="text-5xl font-extrabold col-span-1 text-center">
+      <p className="text-4xl font-extrabold col-span-1 text-center max-md:text-start">
         Recent Projects
       </p>
-      <div className="col-span-1 grid grid-cols-3 gap gap-x-8">
-        <div
-          className={` ${
-            onlyImage.image1
-              ? "grid-cols-1 justify-self-center overflow-hidden"
-              : "grid-cols-1"
-          } col-span-1 grid relative z-0 ease-in duration-300 `}
-          onClick={() =>
-            setOnlyImage({
-              image1: !onlyImage.image1,
-              image2: true,
-              image3: true,
-            })
-          }
-        >
-          <div className={` ${onlyImage.image1 ? "" : "block"} col-span-1  `}>
-            <Image
-              aria-hidden
-              src="/dummy.jpg"
-              alt="project 1"
-              width={1000}
-              height={1000}
-              className=""
-            />
-          </div>
+      <div className="col-span-1 flex flex-wrap gap gap-2 justify-center">
+        {dataProject.map((project) => (
           <div
-            className={` ${
-              onlyImage.image1 ? " opacity-0" : " opacity-100"
-            } col-span-1 absolute left-0 z-1 bg-black bg-opacity-70 h-full p-10 ease-in duration-300 transition-opacity space-y-2 overflow-scroll text-white [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]`}
+            key={project.id}
+            className={` grid-cols-1 col-span-1 grid relative z-0 ease-in duration-300 w-96 h-56justify-self-center overflow-hidden`}
+            onClick={() => {
+              if (project.id == 0) {
+                setOnlyImage({
+                  image1: false,
+                  image2: true,
+                });
+              } else if (project.id == 1) {
+                setOnlyImage({
+                  image1: true,
+                  image2: false,
+                });
+              }
+            }}
           >
-            <p className="text-3xl font-bold">Project 1</p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel
-              egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac
-              sodales id, porttitor vitae est. Donec laoreet rutrum libero sed
-              pharetra. Duis a arcu convallis, gravida purus eget, mollis diam.
-              Praesent non urna non mauris laoreet ultricies eget at enim.
-              Phasellus lacus odio, ullamcorper ac ipsum in, tincidunt tincidunt
-              massa. Suspendisse ut malesuada sapien, vitae mollis diam.
-              Suspendisse tristique et ex non faucibus. Pellentesque a urna
-              risus.
-            </p>
+            <div>
+              <Image
+                aria-hidden
+                src={project.image}
+                alt="project 1"
+                width={1000}
+                height={1000}
+                className="w-96 h-56 object-cover"
+                loading="lazy"
+              />
+            </div>
+            <div
+              className={` ${
+                (
+                  project.id == 0
+                    ? onlyImage.image1
+                    : project.id == 1
+                    ? onlyImage.image2
+                    : true
+                )
+                  ? "opacity-0"
+                  : "opacity-100"
+              }
+              } col-span-1 absolute left-0 z-1 bg-black bg-opacity-70 h-full p-6 ease-in duration-300 transition-opacity space-y-2 overflow-scroll text-white [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]`}
+            >
+              <p
+                className={`${
+                  (
+                    project.id == 0
+                      ? onlyImage.image1
+                      : project.id == 1
+                      ? onlyImage.image2
+                      : true
+                  )
+                    ? "hidden"
+                    : "block"
+                } text-xl font-bold`}
+              >
+                <Link className="" href={project.link}>
+                  {project.namaProject}{" "}
+                  <FontAwesomeIcon
+                    icon={faArrowUpRightFromSquare}
+                    size="2xs"
+                    className="pr-2"
+                  />{" "}
+                </Link>
+              </p>
+              <p className="text-sm">{project.detail}</p>
+            </div>
           </div>
-        </div>
-        <div
-          className={` ${
-            onlyImage.image2
-              ? "grid-cols-1 justify-self-center overflow-hidden"
-              : "grid-cols-1"
-          } col-span-1 grid relative z-0 ease-in duration-300 `}
-          onClick={() =>
-            setOnlyImage({
-              image1: true,
-              image2: !onlyImage.image2,
-              image3: true,
-            })
-          }
-        >
-          <div className={` ${onlyImage.image2 ? "" : "block"} col-span-1  `}>
-            <Image
-              aria-hidden
-              src="/dummy.jpg"
-              alt="project 1"
-              width={1000}
-              height={1000}
-              className=""
-            />
-          </div>
-          <div
-            className={` ${
-              onlyImage.image2 ? " opacity-0" : " opacity-100"
-            } col-span-1 absolute left-0 z-1 bg-black bg-opacity-70 h-full p-10 ease-in duration-300 transition-opacity space-y-2 overflow-scroll text-white [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]`}
-          >
-            <p className="text-3xl font-bold">Project 1</p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel
-              egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac
-              sodales id, porttitor vitae est. Donec laoreet rutrum libero sed
-              pharetra. Duis a arcu convallis, gravida purus eget, mollis diam.
-              Praesent non urna non mauris laoreet ultricies eget at enim.
-              Phasellus lacus odio, ullamcorper ac ipsum in, tincidunt tincidunt
-              massa. Suspendisse ut malesuada sapien, vitae mollis diam.
-              Suspendisse tristique et ex non faucibus. Pellentesque a urna
-              risus.
-            </p>
-          </div>
-        </div>
-        <div
-          className={` ${
-            onlyImage.image3
-              ? "grid-cols-1 justify-self-center overflow-hidden"
-              : "grid-cols-1"
-          } col-span-1 grid relative z-0 ease-in duration-300 `}
-          onClick={() =>
-            setOnlyImage({
-              image1: true,
-              image2: true,
-              image3: !onlyImage.image3,
-            })
-          }
-        >
-          <div className={` ${onlyImage.image3 ? "" : "block"} col-span-1  `}>
-            <Image
-              aria-hidden
-              src="/dummy.jpg"
-              alt="project 1"
-              width={1000}
-              height={1000}
-              className=""
-            />
-          </div>
-          <div
-            className={` ${
-              onlyImage.image3 ? " opacity-0" : " opacity-100"
-            } col-span-1 absolute left-0 z-1 bg-black bg-opacity-70 h-full p-10 ease-in duration-300 transition-opacity space-y-2 overflow-scroll text-white [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]`}
-          >
-            <p className="text-3xl font-bold">Project 1</p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel
-              egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac
-              sodales id, porttitor vitae est. Donec laoreet rutrum libero sed
-              pharetra. Duis a arcu convallis, gravida purus eget, mollis diam.
-              Praesent non urna non mauris laoreet ultricies eget at enim.
-              Phasellus lacus odio, ullamcorper ac ipsum in, tincidunt tincidunt
-              massa. Suspendisse ut malesuada sapien, vitae mollis diam.
-              Suspendisse tristique et ex non faucibus. Pellentesque a urna
-              risus.
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
+      {/* <p className=" col-span-1 pt-4 text-center">
+        <Link
+          href="#"
+          className=" bg-neutral-800 text-white p-3 px-10 hover:bg-white hover:text-black ease-in-out duration-300 "
+        >
+          Another Project
+        </Link>
+      </p> */}
     </div>
   );
 }
